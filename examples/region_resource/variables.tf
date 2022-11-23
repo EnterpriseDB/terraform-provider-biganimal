3bf215f29b27e919c04227d90479e7efc4706c3e
Serdar Dalgic <serdar.dalgic@enterprisedb.com>
2022-11-23 02:16:23 +0100

    style: Remove unused examples, they've been moved to new directories already

--- examples/region_resource/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloud_provider" {
-  type        = string
-  description = "The name of the cloud provider"
-}
-
-variable "region_id" {
-  type        = string
-  description = "region id"
-}
