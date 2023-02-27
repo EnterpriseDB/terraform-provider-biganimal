17741205e3c059aeceba6d2b39b727bd585c982a
Amol Vyavahare <amol.vyavahare@enterprisedb.com>
2023-02-27 17:55:00 +0530

    feat: create fa replica support on azure

--- examples/resources/biganimal_fa_replica/azure/resource.tf
+++ examples/resources/biganimal_fa_replica/azure/resource.tf
@@ -11,15 +11,20 @@ terraform {
   }
 }
 
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
+//resource "random_password" "password" {
+//  length           = 16
+//  special          = true
+//  override_special = "!#$%&*()-_=+[]{}<>:?"
+//}
 
 variable "cluster_name" {
   type        = string
-  description = "The name of the cluster."
+  description = "The name of the faraway replica cluster."
+}
+
+variable "source_cluster_id" {
+  type        = string
+  description = "BigAnimal source cluster ID"
 }
 
 variable "project_id" {
@@ -27,9 +32,10 @@ variable "project_id" {
   description = "BigAnimal Project ID"
 }
 
-resource "biganimal_cluster" "single_node_cluster" {
+resource "biganimal_fareplica" "faraway_replica" {
   cluster_name = var.cluster_name
   project_id   = var.project_id
+  source_cluster_id = var.source_cluster_id
 
   allowed_ip_ranges {
     cidr_block  = "127.0.0.1/32"
@@ -42,39 +48,42 @@ resource "biganimal_cluster" "single_node_cluster" {
   }
 
   backup_retention_period = "6d"
-  cluster_architecture {
-    id    = "single"
-    nodes = 1
-  }
+//  cluster_architecture {
+//    id    = "single"
+//    nodes = 1
+//  }
+
   csp_auth = false
 
   instance_type = "azure:Standard_D2s_v3"
-  password      = resource.random_password.password.result
-  pg_config {
-    name  = "application_name"
-    value = "created through terraform"
-  }
+//  password      = resource.random_password.password.result
 
-  pg_config {
-    name  = "array_nulls"
-    value = "off"
-  }
+  // todo: add some other example for pgConfig, below values are not modifyable for farep
+//  pg_config {
+//    name  = "application_name"
+//    value = "created through terraform"
+//  }
+//
+//  pg_config {
+//    name  = "array_nulls"
+//    value = "off"
+//  }
 
   storage {
     volume_type       = "azurepremiumstorage"
     volume_properties = "P1"
-    size              = "4 Gi"
+//    size              = "4 Gi"
   }
 
-  pg_type               = "epas"
-  pg_version            = "14"
+//  pg_type               = "epas"
+//  pg_version            = "14"
   private_networking    = false
-  cloud_provider        = "azure"
-  read_only_connections = false
-  region                = "eastus2"
+//  cloud_provider        = "azure"
+//  read_only_connections = false
+  region                = "australiaeast"
 }
 
-output "password" {
-  sensitive = true
-  value     = resource.biganimal_cluster.single_node_cluster.password
-}
+//output "password" {
+//  sensitive = true
+//  value     = resource.biganimal_cluster.single_node_cluster.password
+//}
