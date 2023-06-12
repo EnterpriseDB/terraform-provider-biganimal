7f0fbd27f4f7f89bac9389a04e3f1a8c794b3e27
Serdar Dalgic <serdar.dalgic@enterprisedb.com>
2023-06-12 12:05:12 +0200

    feat: Add number_of_projects output to the examples

--- examples/data-sources/biganimal_projects/data-source.tf
+++ examples/data-sources/biganimal_projects/data-source.tf
@@ -3,3 +3,7 @@ data "biganimal_projects" "this" {}
 output "projects" {
   value = data.biganimal_projects.this.projects
 }
+
+output "number_of_projects" {
+  value = length(data.biganimal_projects.this.projects)
+}
