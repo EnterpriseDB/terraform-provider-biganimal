bbd82dfa76a6df31d591cb869103f942c36f0286
Serdar Dalgic <serdar.dalgic@enterprisedb.com>
2023-06-14 12:36:50 +0200

    docs: Improve example docs for projects data-source

--- examples/data-sources/biganimal_projects/data-source.tf
+++ examples/data-sources/biganimal_projects/data-source.tf
@@ -1,4 +1,6 @@
-data "biganimal_projects" "this" {}
+data "biganimal_projects" "this" {
+  query = var.query
+}
 
 output "projects" {
   value = data.biganimal_projects.this.projects
@@ -6,4 +8,10 @@ output "projects" {
 
 output "number_of_projects" {
   value = length(data.biganimal_projects.this.projects)
+}
+
+variable "query" {
+  type        = string
+  description = "Query string for the projects"
+  default     = ""
 }
