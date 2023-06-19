c382d5955b714ecab0ca4e038be2ba0d887cbac3
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2023-06-19 11:07:13 +0100

    feat: catch up to main

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
-
 	"github.com/EnterpriseDB/terraform-provider-biganimal/pkg/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
@@ -13,9 +11,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
 	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"log"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
 // ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ./examples/
 
@@ -23,10 +24,14 @@ import (
 // can be customized.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
-// these will be set by the goreleaser configuration
-// to appropriate values for the compiled binary
-var version string = "dev" // goreleaser can also pass the specific commit if you want
-// commit  string = ""
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+
+	// goreleaser can also pass the specific commit if you want
+	// commit  string = ""
+)
 
 func main() {
 	var debugMode bool
