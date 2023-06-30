2ef7377245087c88ba68db32ba951db4650ee51a
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2023-06-30 13:00:38 +0100

    feat: wip checkpoint datagroups groups changed to use nested attributes and not blocks

--- examples/resources/biganimal_pgd/resource.tf
+++ examples/resources/biganimal_pgd/resource.tf
@@ -31,96 +31,106 @@ resource "biganimal_pgd" "pgd_cluster" {
   cluster_name = var.cluster_name
   project_id   = var.project_id
   password     = resource.random_password.password.result
-  data_groups {
-    allowed_ip_ranges {
-      cidr_block  = "127.0.0.1/32"
-      description = "localhost"
+  data_groups = [
+    {
+      allowed_ip_ranges = [
+        {
+          cidr_block  = "127.0.0.1/32"
+          description = "localhost"
+        },
+        {
+          cidr_block  = "192.168.0.1/32"
+          description = "description!"
+        },
+      ]
+      backup_retention_period = "6d"
+      cluster_architecture = {
+        cluster_architecture_id = "pgd"
+        nodes                   = 2
+      }
+      csp_auth = false
+      instance_type = {
+        instance_type_id = "azure:Standard_D2s_v3"
+      }
+      pg_config = [
+        {
+          name  = "application_name"
+          value = "created through terraform"
+        },
+        {
+          name  = "array_nulls"
+          value = "off"
+        },
+      ]
+      storage = {
+        volume_type       = "azurepremiumstorage"
+        volume_properties = "P1"
+        size              = "4 Gi"
+      }
+      pg_type = {
+        pg_type_id = "epas"
+      }
+      pg_version = {
+        pg_version_id = "14"
+      }
+      private_networking = false
+      cloud_provider = {
+        cloud_provider_id = "azure"
+      }
+      region = {
+        region_id = "northeurope"
+      }
+    },
+{
+      allowed_ip_ranges = [
+        {
+          cidr_block  = "127.0.0.1/32"
+          description = "localhost"
+        },
+        {
+          cidr_block  = "192.168.0.1/32"
+          description = "description!"
+        },
+      ]
+      backup_retention_period = "6d"
+      cluster_architecture = {
+        cluster_architecture_id = "pgd"
+        nodes                   = 2
+      }
+      csp_auth = false
+      instance_type = {
+        instance_type_id = "azure:Standard_D2s_v3"
+      }
+      pg_config = [
+        {
+          name  = "application_name"
+          value = "created through terraform"
+        },
+        {
+          name  = "array_nulls"
+          value = "off"
+        },
+      ]
+      storage = {
+        volume_type       = "azurepremiumstorage"
+        volume_properties = "P1"
+        size              = "4 Gi"
+      }
+      pg_type = {
+        pg_type_id = "epas"
+      }
+      pg_version = {
+        pg_version_id = "14"
+      }
+      private_networking = false
+      cloud_provider = {
+        cloud_provider_id = "azure"
+      }
+      region = {
+        region_id = "eastus"
+      }
     }
-    allowed_ip_ranges {
-      cidr_block  = "192.168.0.1/32"
-      description = "description!"
-    }
-    backup_retention_period = "6d"
-    cluster_architecture {
-      cluster_architecture_id = "pgd"
-      nodes                   = 2
-    }
-    csp_auth = false
-    instance_type {
-      instance_type_id = "azure:Standard_D2s_v3"
-    }
-    pg_config {
-      name  = "application_name"
-      value = "created through terraform"
-    }
-    pg_config {
-      name  = "array_nulls"
-      value = "off"
-    }
-    storage {
-      volume_type       = "azurepremiumstorage"
-      volume_properties = "P1"
-      size              = "4 Gi"
-    }
-    pg_type {
-      pg_type_id = "epas"
-    }
-    pg_version {
-      pg_version_id = "14"
-    }
-    private_networking = false
-    cloud_provider {
-      cloud_provider_id = "azure"
-    }
-    region {
-      region_id = "northeurope"
-    }
-  }
-  data_groups {
-    allowed_ip_ranges {
-      cidr_block  = "127.0.0.1/32"
-      description = "localhost"
-    }
-    allowed_ip_ranges {
-      cidr_block  = "192.168.0.1/32"
-      description = "description!"
-    }
-    backup_retention_period = "6d"
-    cluster_architecture {
-      cluster_architecture_id = "pgd"
-      nodes                   = 2
-    }
-    csp_auth = false
-    instance_type {
-      instance_type_id = "azure:Standard_D2s_v3"
-    }
-    pg_config {
-      name  = "application_name"
-      value = "created through terraform"
-    }
-    pg_config {
-      name  = "array_nulls"
-      value = "off"
-    }
-    storage {
-      volume_type       = "azurepremiumstorage"
-      volume_properties = "P1"
-      size              = "4 Gi"
-    }
-    pg_type {
-      pg_type_id = "epas"
-    }
-    pg_version {
-      pg_version_id = "14"
-    }
-    private_networking = false
-    cloud_provider {
-      cloud_provider_id = "azure"
-    }
-    region {
-      region_id = "eastus"
-    }
-  }
+  ]
   witness_groups = [
     {
       region = {
@@ -128,4 +138,5 @@ resource "biganimal_pgd" "pgd_cluster" {
       }
     }
   ]
+  
 }
--- pkg/provider/resource_pgd.go
+++ pkg/provider/resource_pgd.go
@@ -27,159 +27,41 @@ func (p pgdResource) Metadata(ctx context.Context, req resource.MetadataRequest,
 func (p pgdResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		MarkdownDescription: "The PGD cluster data source describes a BigAnimal cluster. The data source requires your PGD cluster name.",
-		Blocks: map[string]schema.Block{
-			"data_groups": schema.SetNestedBlock{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"project_id": schema.StringAttribute{
+				Description: "BigAnimal Project ID.",
+				Optional:    true,
+			},
+			"cluster_id": schema.StringAttribute{
+				Description: "Cluster ID.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"cluster_name": schema.StringAttribute{
+				Description: "cluster name",
+				Required:    true,
+			},
+			"most_recent": schema.BoolAttribute{
+				Description: "Show the most recent cluster when there are multiple clusters with the same name",
+				Optional:    true,
+			},
+			"password": schema.StringAttribute{
+				Description: "Password for the user edb_admin. It must be 12 characters or more.",
+				Required:    true,
+				Sensitive:   true,
+			},
+			"data_groups": schema.SetNestedAttribute{
 				Description: "Cluster data groups.",
-				NestedObject: schema.NestedBlockObject{
-					Blocks: map[string]schema.Block{
-						"allowed_ip_ranges": schema.SetNestedBlock{
-							Description: "Allowed IP ranges.",
-							NestedObject: schema.NestedBlockObject{
-								Attributes: map[string]schema.Attribute{
-									"cidr_block": schema.StringAttribute{
-										Description: "CIDR block",
-										Required:    true,
-									},
-									"description": schema.StringAttribute{
-										Description: "Description of CIDR block",
-										Optional:    true,
-									},
-								},
-							},
-						},
-						"pg_config": schema.SetNestedBlock{
-							Description: "Database configuration parameters.",
-
-							NestedObject: schema.NestedBlockObject{
-								Attributes: map[string]schema.Attribute{
-									"name": schema.StringAttribute{
-										Description: "GUC name.",
-										Optional:    true,
-									},
-									"value": schema.StringAttribute{
-										Description: "GUC value.",
-										Optional:    true,
-									},
-								},
-							},
-						},
-						"cluster_architecture": schema.SingleNestedBlock{
-							Attributes: map[string]schema.Attribute{
-								"cluster_architecture_id": schema.StringAttribute{
-									Description: "Cluster architecture ID.",
-									Required:    true,
-								},
-								"cluster_architecture_name": schema.StringAttribute{
-									Description: "Name.",
-									Computed:    true,
-									PlanModifiers: []planmodifier.String{
-										stringplanmodifier.UseStateForUnknown(),
-									},
-								},
-								"nodes": schema.Float64Attribute{
-									Description: "Node count.",
-									Required:    true,
-								},
-								"witness_nodes": schema.Float64Attribute{
-									Description: "Witness nodes count.",
-									Optional:    true,
-								},
-							},
-						},
-						"storage": schema.SingleNestedBlock{
-							Description: "Storage.",
-							Attributes: map[string]schema.Attribute{
-								"iops": schema.StringAttribute{
-									Description: "IOPS for the selected volume.",
-									Optional:    true,
-									Computed:    true,
-								},
-								"size": schema.StringAttribute{
-									Description: "Size of the volume.",
-									Optional:    true,
-									Computed:    true,
-								},
-								"throughput": schema.StringAttribute{
-									Description: "Throughput.",
-									Optional:    true,
-									Computed:    true,
-								},
-								"volume_properties": schema.StringAttribute{
-									Description: "Volume properties.",
-									Required:    true,
-								},
-								"volume_type": schema.StringAttribute{
-									Description: "Volume type.",
-									Required:    true,
-								},
-							},
-						},
-						"pg_type": schema.SingleNestedBlock{
-							Description: "Postgres type.",
-							Attributes: map[string]schema.Attribute{
-								"pg_type_id": schema.StringAttribute{
-									Description: "Data group pg type id.",
-									Optional:    true,
-								},
-							},
-						},
-						"pg_version": schema.SingleNestedBlock{
-							Description: "Postgres version.",
-							Attributes: map[string]schema.Attribute{
-								"pg_version_id": schema.StringAttribute{
-									Description: "Data group pg version id.",
-									Optional:    true,
-								},
-							},
-						},
-						"cloud_provider": schema.SingleNestedBlock{
-							Description: "Cloud provider.",
-							Attributes: map[string]schema.Attribute{
-								"cloud_provider_id": schema.StringAttribute{
-									Description: "Data group cloud provider id.",
-									Optional:    true,
-								},
-							},
-						},
-						"region": schema.SingleNestedBlock{
-							Description: "Region.",
-							Attributes: map[string]schema.Attribute{
-								"region_id": schema.StringAttribute{
-									Description: "Data group region id.",
-									Optional:    true,
-								},
-							},
-						},
-						"instance_type": schema.SingleNestedBlock{
-							Description: "Instance type.",
-							Attributes: map[string]schema.Attribute{
-								"instance_type_id": schema.StringAttribute{
-									Description: "Data group instance type id.",
-									Optional:    true,
-								},
-							},
-						},
-						// "maintenance_window": schema.SingleNestedBlock{
-						// 	Description: "Custom maintenance window.",
-						// 	Attributes: map[string]schema.Attribute{
-						// 		"is_enabled": schema.BoolAttribute{
-						// 			Description: "Is maintenance window enabled.",
-						// 			Optional:    true,
-						// 			Computed:    true,
-						// 		},
-						// 		"start_day": schema.StringAttribute{
-						// 			Description: "Start day.",
-						// 			Optional:    true,
-						// 			Computed:    true,
-						// 		},
-						// 		"start_time": schema.StringAttribute{
-						// 			Description: "Start time.",
-						// 			Optional:    true,
-						// 			Computed:    true,
-						// 		},
-						// 	},
-						// },
-					},
+				Required:    true,
+				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"group_id": schema.StringAttribute{
 							Description: "Group ID of the group.",
@@ -264,131 +146,164 @@ func (p pgdResource) Schema(ctx context.Context, req resource.SchemaRequest, res
 							Computed:    true,
 							ElementType: types.StringType,
 						},
+						"allowed_ip_ranges": schema.SetNestedAttribute{
+							Description: "Allowed IP ranges.",
+							Optional:    true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"cidr_block": schema.StringAttribute{
+										Description: "CIDR block",
+										Required:    true,
+									},
+									"description": schema.StringAttribute{
+										Description: "Description of CIDR block",
+										Optional:    true,
+									},
+								},
+							},
+						},
+						"pg_config": schema.SetNestedAttribute{
+							Description: "Database configuration parameters.",
+							Optional:    true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"name": schema.StringAttribute{
+										Description: "GUC name.",
+										Required:    true,
+									},
+									"value": schema.StringAttribute{
+										Description: "GUC value.",
+										Required:    true,
+									},
+								},
+							},
+						},
+						"cluster_architecture": schema.SingleNestedAttribute{
+							Description: "Cluster architecture.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"cluster_architecture_id": schema.StringAttribute{
+									Description: "Cluster architecture ID.",
+									Required:    true,
+								},
+								"cluster_architecture_name": schema.StringAttribute{
+									Description: "Name.",
+									Computed:    true,
+									PlanModifiers: []planmodifier.String{
+										stringplanmodifier.UseStateForUnknown(),
+									},
+								},
+								"nodes": schema.Float64Attribute{
+									Description: "Node count.",
+									Required:    true,
+								},
+								"witness_nodes": schema.Float64Attribute{
+									Description: "Witness nodes count.",
+									Optional:    true,
+								},
+							},
+						},
+						"storage": schema.SingleNestedAttribute{
+							Description: "Storage.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"iops": schema.StringAttribute{
+									Description: "IOPS for the selected volume.",
+									Optional:    true,
+									Computed:    true,
+								},
+								"size": schema.StringAttribute{
+									Description: "Size of the volume.",
+									Optional:    true,
+									Computed:    true,
+								},
+								"throughput": schema.StringAttribute{
+									Description: "Throughput.",
+									Optional:    true,
+									Computed:    true,
+								},
+								"volume_properties": schema.StringAttribute{
+									Description: "Volume properties.",
+									Required:    true,
+								},
+								"volume_type": schema.StringAttribute{
+									Description: "Volume type.",
+									Required:    true,
+								},
+							},
+						},
+						"pg_type": schema.SingleNestedAttribute{
+							Description: "Postgres type.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"pg_type_id": schema.StringAttribute{
+									Description: "Data group pg type id.",
+									Required:    true,
+								},
+							},
+						},
+						"pg_version": schema.SingleNestedAttribute{
+							Description: "Postgres version.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"pg_version_id": schema.StringAttribute{
+									Description: "Data group pg version id.",
+									Required:    true,
+								},
+							},
+						},
+						"cloud_provider": schema.SingleNestedAttribute{
+							Description: "Cloud provider.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"cloud_provider_id": schema.StringAttribute{
+									Description: "Data group cloud provider id.",
+									Required:    true,
+								},
+							},
+						},
+						"region": schema.SingleNestedAttribute{
+							Description: "Region.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"region_id": schema.StringAttribute{
+									Description: "Data group region id.",
+									Required:    true,
+								},
+							},
+						},
+						"instance_type": schema.SingleNestedAttribute{
+							Description: "Instance type.",
+							Required:    true,
+							Attributes: map[string]schema.Attribute{
+								"instance_type_id": schema.StringAttribute{
+									Description: "Data group instance type id.",
+									Required:    true,
+								},
+							},
+						},
+						// "maintenance_window": schema.SingleNestedBlock{
+						// 	Description: "Custom maintenance window.",
+						// 	Attributes: map[string]schema.Attribute{
+						// 		"is_enabled": schema.BoolAttribute{
+						// 			Description: "Is maintenance window enabled.",
+						// 			Optional:    true,
+						// 			Computed:    true,
+						// 		},
+						// 		"start_day": schema.StringAttribute{
+						// 			Description: "Start day.",
+						// 			Optional:    true,
+						// 			Computed:    true,
+						// 		},
+						// 		"start_time": schema.StringAttribute{
+						// 			Description: "Start time.",
+						// 			Optional:    true,
+						// 			Computed:    true,
+						// 		},
+						// 	},
+						// },
 					},
 				},
-			},
-			// "witness_groups": schema.SetNestedBlock{
-			// 	Description: "Cluster witness groups.",
-			// 	NestedObject: schema.NestedBlockObject{
-			// 		Blocks: map[string]schema.Block{
-			// 			"cluster_architecture": schema.SingleNestedBlock{
-			// 				Attributes: map[string]schema.Attribute{
-			// 					"cluster_architecture_id": schema.StringAttribute{
-			// 						Description: "Cluster architecture ID.",
-			// 						Optional:    true,
-			// 					},
-			// 					"cluster_architecture_name": schema.StringAttribute{
-			// 						Description: "Name.",
-			// 						Optional:    true,
-			// 					},
-			// 					"nodes": schema.Float64Attribute{
-			// 						Description: "Nodes.",
-			// 						Optional:    true,
-			// 					},
-			// 					"witness_nodes": schema.Float64Attribute{
-			// 						Description: "Witness nodes count.",
-			// 						Optional:    true,
-			// 					},
-			// 				},
-			// 			},
-			// 			"instance_type": schema.SingleNestedBlock{
-			// 				Description: "Instance type.",
-			// 				Attributes: map[string]schema.Attribute{
-			// 					"instance_type_id": schema.StringAttribute{
-			// 						Description: "Witness group instance type id.",
-			// 						Computed:    true,
-			// 						PlanModifiers: []planmodifier.String{
-			// 							stringplanmodifier.UseStateForUnknown(),
-			// 						},
-			// 					},
-			// 				},
-			// 			},
-			// 			"storage": schema.SingleNestedBlock{
-			// 				Description: "Storage.",
-			// 				Attributes: map[string]schema.Attribute{
-			// 					"iops": schema.StringAttribute{
-			// 						Description: "IOPS for the selected volume.",
-			// 						Optional:    true,
-			// 					},
-			// 					"size": schema.StringAttribute{
-			// 						Description: "Size of the volume.",
-			// 						Optional:    true,
-			// 					},
-			// 					"throughput": schema.StringAttribute{
-			// 						Description: "Throughput.",
-			// 						Optional:    true,
-			// 					},
-			// 					"volume_properties": schema.StringAttribute{
-			// 						Description: "Volume properties.",
-			// 						Optional:    true,
-			// 					},
-			// 					"volume_type": schema.StringAttribute{
-			// 						Description: "Volume type.",
-			// 						Optional:    true,
-			// 					},
-			// 				},
-			// 			},
-			// 		},
-			// 		Attributes: map[string]schema.Attribute{
-			// 			"cluster_type": schema.StringAttribute{
-			// 				Description: "Type of the Specified Cluster",
-			// 				Optional:    true,
-			// 				Computed:    true,
-			// 			},
-			// 			"cloud_provider": schema.SingleNestedAttribute{
-			// 				Description: "Cloud provider.",
-			// 				Computed:    true,
-			// 				Attributes: map[string]schema.Attribute{
-			// 					"cloud_provider_id": schema.StringAttribute{
-			// 						Description: "Cloud provider id.",
-			// 						Computed:    true,
-			// 					},
-			// 				},
-			// 			},
-			// 			"region": schema.SingleNestedAttribute{
-			// 				Description: "Region.",
-			// 				Required:    true,
-			// 				Attributes: map[string]schema.Attribute{
-			// 					"region_id": schema.StringAttribute{
-			// 						Description: "Region id.",
-			// 						Required:    true,
-			// 					},
-			// 				},
-			// 			},
-			// 		},
-			// 	},
-			// },
-		},
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-			},
-			"project_id": schema.StringAttribute{
-				Description: "BigAnimal Project ID.",
-				Optional:    true,
-			},
-			"cluster_id": schema.StringAttribute{
-				Description: "Cluster ID.",
-				Computed:    true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-			},
-			"cluster_name": schema.StringAttribute{
-				Description: "cluster name",
-				Required:    true,
-			},
-			"most_recent": schema.BoolAttribute{
-				Description: "Show the most recent cluster when there are multiple clusters with the same name",
-				Optional:    true,
-			},
-			"password": schema.StringAttribute{
-				Description: "Password for the user edb_admin. It must be 12 characters or more.",
-				Required:    true,
-				Sensitive:   true,
 			},
 			"witness_groups": schema.SetNestedAttribute{
 				Optional: true,
@@ -573,18 +488,6 @@ func (p pgdResource) Create(ctx context.Context, req resource.CreateRequest, res
 		fmt.Println(err)
 	}
 	fmt.Print(string(bb))
-
-	www := config.WitnessGroups[0]
-
-	config.WitnessGroups = []pgd.WitnessGroup{
-		{
-			Region: &pgd.Region{
-				RegionId: "canadacentral",
-			},
-			ClusterType: www.ClusterType,
-			Provider:    www.Provider,
-		},
-	}
 
 	diags = resp.State.Set(ctx, &config)
 	resp.Diagnostics.Append(diags...)
