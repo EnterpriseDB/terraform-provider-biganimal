c37e857ec0a65e3741920d67c8f5886cb8b4f283
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2023-07-20 15:54:04 +0100

    feat: allowed ips fixed. using [] in config returns 0.0.0.0/0

--- pkg/plan_modifier/allowed_ips.go
+++ pkg/plan_modifier/allowed_ips.go
@@ -3,8 +3,9 @@ package plan_modifier
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 func CustomAllowedIps() planmodifier.Set {
@@ -27,7 +28,14 @@ func (m customAllowedIpsModifier) MarkdownDescription(_ context.Context) string 
 // PlanModifyList implements the plan modification logic.
 func (m customAllowedIpsModifier) PlanModifySet(ctx context.Context, req planmodifier.SetRequest, resp *planmodifier.SetResponse) {
 	if len(resp.PlanValue.Elements()) == 0 {
-		resp.PlanValue = types.SetNull(resp.PlanValue.ElementType(ctx))
+		defaultAttrs := map[string]attr.Value{"cidr_block": basetypes.NewStringValue("0.0.0.0/0"), "description": basetypes.NewStringValue("")}
+		defaultAttrTypes := map[string]attr.Type{"cidr_block": defaultAttrs["cidr_block"].Type(ctx), "description": defaultAttrs["description"].Type(ctx)}
+
+		defaultObjectValue := basetypes.NewObjectValueMust(defaultAttrTypes, defaultAttrs)
+		setOfObjects := []attr.Value{}
+		setOfObjects = append(setOfObjects, defaultObjectValue)
+		setValue := basetypes.NewSetValueMust(defaultObjectValue.Type(ctx), setOfObjects)
+		resp.PlanValue = setValue
 		return
 	}
 
