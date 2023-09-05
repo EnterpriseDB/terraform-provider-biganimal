3843abe436cdfc6b2022e9254b530c2800811677
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2023-09-05 15:20:07 +0100

    feat: plan modifier maintenance window fix for is_enabled. Removing CMW will set CMW to off

--- pkg/plan_modifier/maintenance_window.go
+++ pkg/plan_modifier/maintenance_window.go
@@ -2,10 +2,11 @@ package plan_modifier
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 func MaintenanceWindowForUnknown() planmodifier.Object {
@@ -34,34 +35,20 @@ func (m MaintenanceWindowForUnknownModifier) PlanModifyObject(ctx context.Contex
 			startDayAttr := planAttr["start_day"]
 			startTimeAttr := planAttr["start_time"]
 
-			if strings.Replace(startDayAttr.String(), "\"", "", -1) != "0" && !startDayAttr.IsNull() && !startDayAttr.IsUnknown() {
-				resp.Diagnostics.AddError("Maintenance window start_day cannot be set if is_enabled is false", fmt.Sprintf("maintenance window start_day has changed to %v, please set to 0 or remove",
-					startDayAttr))
-			}
-
-			if strings.Replace(startTimeAttr.String(), "\"", "", -1) != "00:00" && !startTimeAttr.IsNull() && !startTimeAttr.IsUnknown() {
-				resp.Diagnostics.AddError("Maintenance window start_time cannot be set if is_enabled is false", fmt.Sprintf("maintenance window start_time has changed to %v, please set to 00:00 or remove",
-					startTimeAttr))
+			if (strings.Replace(startDayAttr.String(), "\"", "", -1) != "0" && !startDayAttr.IsUnknown()) ||
+				(strings.Replace(startTimeAttr.String(), "\"", "", -1) != "00:00" && !startTimeAttr.IsUnknown()) {
+				resp.Diagnostics.AddError("Maintenance window start_day and start_time cannot be set if is_enabled is false", "Please either remove or comment out start_time and start_day values or the whole maintenance_window block.")
 			}
 
 			return
 		}
 	}
 
-	// // Do nothing if there is no state value.
-	// if req.StateValue.IsNull() {
-	// 	return
-	// }
-
-	// // Do nothing if there is a known planned value.
-	// if !req.PlanValue.IsUnknown() {
-	// 	return
-	// }
-
-	// // Do nothing if there is an unknown configuration value, otherwise interpolation gets messed up.
-	// if req.ConfigValue.IsUnknown() {
-	// 	return
-	// }
-
-	// resp.PlanValue = req.StateValue
+	if resp.PlanValue.IsUnknown() {
+		resp.PlanValue = basetypes.NewObjectValueMust(resp.PlanValue.AttributeTypes(ctx), map[string]attr.Value{
+			"is_enabled": basetypes.NewBoolValue(false),
+			"start_day":  basetypes.NewInt64Value(0),
+			"start_time": basetypes.NewStringValue("00:00"),
+		})
+	}
 }
