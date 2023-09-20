e83fa7df8bc2630244a5e4c04054780d5371a2aa
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2023-09-20 14:46:47 +0100

    fix: PGD add data group expects cluster arch witness nodes unknowns. This is reflect a change from the API

--- pkg/models/pgd/terraform/cluster_architecture.go
+++ pkg/models/pgd/terraform/cluster_architecture.go
@@ -6,5 +6,5 @@ type ClusterArchitecture struct {
 	ClusterArchitectureId   string       `tfsdk:"cluster_architecture_id"`
 	ClusterArchitectureName types.String `tfsdk:"cluster_architecture_name"`
 	Nodes                   float64      `tfsdk:"nodes"`
-	WitnessNodes            *float64     `tfsdk:"witness_nodes"`
+	WitnessNodes            types.Int64  `tfsdk:"witness_nodes"`
 }
