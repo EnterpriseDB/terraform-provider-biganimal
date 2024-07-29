a268c50024f40a620987933ea37e0f3c69585c1e
wai-wong-edb <119956756+wai-wong-edb@users.noreply.github.com>
2024-07-29 13:18:44 +0100

    feat: v1 migrate faraway replica (#555)
    * feat: migrate faraway replica to terraform framework
    
    * fix: examples refactored removed bah and combined with cloud provider examples
    
    * feat: migrate faraway replica update to terraform framework
    
    * feat: examples and import state and delete
    
    * fix: removed wrong checkin
    
    * fix: examples
    
    (cherry picked from commit 037c5ef750496e980b38756be143dcf1df665f73)
    
    * fix: examples bah fix
    
    * fix: examples

--- examples/resources/biganimal_faraway_replica/cluster_and_faraway_replica/resource.tf
+++ examples/resources/biganimal_faraway_replica/cluster_and_faraway_replica/resource.tf
@@ -61,46 +61,35 @@ resource "biganimal_faraway_replica" "faraway_replica" {
   project_id        = var.project_id
   source_cluster_id = resource.biganimal_cluster.single_node_cluster.cluster_id
 
-  allowed_ip_ranges {
-    cidr_block  = "127.0.0.1/32"
-    description = "localhost"
-  }
+  allowed_ip_ranges = [
+    {
+      cidr_block  = "127.0.0.1/32"
+      description = "localhost"
+    },
+    {
+      cidr_block  = "192.168.0.1/32"
+      description = "description!"
+    },
+  ]
 
-  allowed_ip_ranges {
-    cidr_block  = "192.168.0.1/32"
-    description = "description!"
-  }
-
-  backup_retention_period = "6d"
+  backup_retention_period = "8d"
   csp_auth                = false
-  instance_type           = "azure:Standard_D2s_v3"
-
+  instance_type           = "aws:c6i.large"
 
   // only following pg_config parameters are configurable for faraway replica
   // max_connections, max_locks_per_transaction, max_prepared_transactions, max_wal_senders, max_worker_processes.
   // it is highly recommended setting these values to be equal to or greater than the source cluster's.
   // Please visit [this page](https://www.enterprisedb.com/docs/biganimal/latest/using_cluster/managing_replicas/#modify-a-faraway-replica)for best practices.
-  pg_config {
-    name  = "max_connections"
-    value = "100"
-  }
-
-  pg_config {
-    name  = "max_locks_per_transaction"
-    value = "64"
-  }
-  pg_config {
-    name  = "max_prepared_transactions"
-    value = "0"
-  }
-  pg_config {
-    name  = "max_wal_senders"
-    value = "10"
-  }
-  pg_config {
-    name  = "max_worker_processes"
-    value = "32"
-  }
+  pg_config = [
+    {
+      name  = "max_connections"
+      value = "100"
+    },
+    {
+      name  = "max_locks_per_transaction"
+      value = "64"
+    }
+  ]
 
   storage {
     volume_type       = "azurepremiumstorage"
