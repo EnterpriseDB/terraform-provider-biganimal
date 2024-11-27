255c714b96a78726749e8bdcffd63aeeeadef951
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2024-11-27 19:02:19 +0000

    fix: validation for principal ids pgd bug fix and update validation message

--- pkg/plan_modifier/cloud_provider.go
+++ pkg/plan_modifier/cloud_provider.go
@@ -36,14 +36,14 @@ func (m customCloudProviderModifier) PlanModifyString(ctx context.Context, req p
 		peIds, ok := configObject["pe_allowed_principal_ids"]
 		if ok && !peIds.IsNull() {
 			resp.Diagnostics.AddError("your cloud account 'pe_allowed_principal_ids' field not allowed error",
-				"field 'pe_allowed_principal_ids' should only be set if you are using 'bah' cloud provider, please remove 'pe_allowed_principal_ids'")
+				"field 'pe_allowed_principal_ids' should only be set if you are using BigAnimal's cloud account e.g. 'bah:aws', please remove 'pe_allowed_principal_ids'")
 			return
 		}
 
 		saIds, ok := configObject["service_account_ids"]
 		if ok && !saIds.IsNull() {
 			resp.Diagnostics.AddError("your cloud account 'service_account_ids' field not allowed error",
-				"field 'service_account_ids' should only be set if you are using cloud provider 'bah:gcp', please remove 'service_account_ids'")
+				"field 'service_account_ids' should only be set if you are using BigAnimal's cloud account 'bah:gcp', please remove 'service_account_ids'")
 			return
 		}
 	}
